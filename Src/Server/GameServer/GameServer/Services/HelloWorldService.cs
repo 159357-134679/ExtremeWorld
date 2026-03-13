1cafbe375d114f74ad89fe42cc08b82b541f8b92
Sun <3254514082@qq.com>
2026-03-13 23:43:42 +0800

    测试用的

---
+++ Src/Server/GameServer/GameServer/Services/HelloWorldService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameServer.Services
+{
+        public void Init()
+        {
+            entities = new ExtremeWorldEntities();
+        }
+    public void start()
+    {
+
+    }
+    public void stop()
+    {
+
+    }
+
+}
